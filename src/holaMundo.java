ee9aa4353894dcbcb858b31d69aa6c73c4361fa7
Andre Santos de Castelo <santosdecastelo@gmail.com>
2022-07-28 20:37:08 +0200

    He añadido tres strings

--- src/holaMundo.java
+++ src/holaMundo.java
@@ -1,7 +1,7 @@
 public class holaMundo {
     public static void main(String[] args) {
         //Esto imprimirá Hello World
-        System.out.println("Hola");
+        System.out.println("Hola o Hello");
 
         String iLove = new String ("I Love Programing");
         String everyDay = new String ("Every Day");
